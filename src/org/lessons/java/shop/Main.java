b46164b29f132fe671947c723058c8d3fa061e61
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-10-20 15:26:23 +0200

    esercizio 1

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,6 +2,17 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
+        //creo dei prodotti
+        Prodotto prodotto1 = new Prodotto("Palla", "fantastica palla da basket", 30.0, 22.0);
+        Prodotto prodotto2 = new Prodotto("Racchetta", "Racchetta da tennis", 180.0, 22);
+        Prodotto prodotto3 = new Prodotto("Arnica", "Pomata per contratture", 20.0, 10.0);
 
+//stampo i prodotti
+        System.out.println(prodotto1.getNome() + ": prezzo senza IVA:" + prodotto1.getPrezzo() + "€" + " prezzo con IVA: " + prodotto1.getPrezzoIva() + "€");
+        System.out.println("Prodotto1: " + prodotto1.getNomeCompleto());
+        System.out.println(prodotto2.getNome() + ": prezzo senza IVA:" + prodotto2.getPrezzo() + "€" + " prezzo con IVA: " + prodotto2.getPrezzoIva() + "€");
+        System.out.println("Prodotto2: " + prodotto2.getNomeCompleto());
+        System.out.println(prodotto3.getNome() + ": prezzo senza IVA:" + prodotto3.getPrezzo() + "€" + " prezzo con IVA: " + prodotto3.getPrezzoIva() + "€");
+        System.out.println("Prodotto3: " + prodotto3.getNomeCompleto());
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,90 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
 
+
+    /**
+     * Nel package org.lessons.java.shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+     * Un prodotto è caratterizzato da:
+     * - codice (numero intero)
+     * - nome
+     * - descrizione
+     * - prezzo
+     * - iva
+     * Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+     * alla creazione di un nuovo prodotto venga passato il valore di tutti gli attributi, tranne il codice, che deve essere valorizzato con un numero random
+     * Il codice prodotto sia accessibile solo in lettura
+     * Gli altri attributi siano accessibili sia in lettura che in scrittura
+     * Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+     * Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+     * Nello stesso package aggiungete una classe Main con metodo main nella quale create tre prodotti e ne stampate il nome completo e il prezzo comprensivo di iva.
+     * BONUS: nella classe Prodotto create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+     */
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private double iva;
+
+    public Prodotto(String nome, String descrizione, double prezzo, double iva) {
+        this.codice = generaRandomCodice();
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    public int generaRandomCodice() {
+        Random random = new Random();
+        return random.nextInt(100000000);
+
+    }
+
+    //metodo per calcolare iva
+    public double getPrezzoIva() {
+        return prezzo + (prezzo * iva / 100);
+    }
+
+    //metodo per stampare il tutto
+    public String getNomeCompleto() {
+        return codice + "-" + nome;
+    }
 }
