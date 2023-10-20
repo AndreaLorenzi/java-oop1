2521e0c70ffa88d386f9e044d49163711ad18a55
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-10-20 17:44:41 +0200

    exercise 2

--- src/org/lessons/java/bank/Bank.java
+++ src/org/lessons/java/bank/Bank.java
@@ -1,7 +1,50 @@
 package org.lessons.java.bank;
 
+import java.util.Scanner;
+
 public class Bank {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci il tuo nome: ");
+        String nomeUtente = scanner.nextLine();
+
+        Conto conto = new Conto(nomeUtente);
+        System.out.println("Queste sono le info del tuo conto: ");
+        System.out.println(conto.getInfoBankaccount() + conto.getSaldoFormatted());
+        int scelta = 0;
+        while (scelta != 4) {
+            System.out.println("Menu:");
+            System.out.println("1. Versa una somma di denaro");
+            System.out.println("2. Preleva una somma di denaro");
+            System.out.println("3. Visualizza il tuo saldo contabilizzato ");
+            System.out.println("4. Esci");
+            System.out.print("Scelta: ");
+            scelta = scanner.nextInt();
+
+            switch (scelta) {
+                case 1:
+                    System.out.print("Inserisci la somma da versare: ");
+                    double versamento = scanner.nextDouble();
+                    conto.versamento(versamento);
+                    System.out.println("Saldo attuale: " + conto.getSaldoFormatted());
+                    break;
+                case 2:
+                    System.out.print("Inserisci la somma da prelevare: ");
+                    double prelievo = scanner.nextDouble();
+                    conto.prelievo(prelievo);
+                    System.out.println("Saldo attuale: " + conto.getSaldoFormatted());
+                    break;
+                case 3:
+                    System.out.println("visualizza il tuo saldo");
+                    System.out.println("Saldo attuale: " + conto.getSaldoFormatted());
+                case 4:
+                    System.out.println("Grazie per averci scelto. Arrivederci!");
+                    break;
+                default:
+                    System.out.println("Scelta non valida. Riprova.");
+            }
+        }
 
     }
 }
--- src/org/lessons/java/bank/Conto.java
+++ src/org/lessons/java/bank/Conto.java
@@ -1,4 +1,64 @@
 package org.lessons.java.bank;
 
+import java.util.Random;
+
 public class Conto {
+    private final int numeroConto;
+    private String titolareConto;
+    private double saldoConto = 0.0;
+
+    //costruttore
+    public Conto(String titolareConto) {
+        this.numeroConto = generatoreRandomNumeroConto();
+        this.titolareConto = titolareConto;
+        this.saldoConto = 0.0;
+        //generatore per creare un numero di conto random
+    }
+
+    private int generatoreRandomNumeroConto() {
+        Random rand = new Random();
+        return rand.nextInt(1000) + 1;
+    }
+
+    public int getNumeroConto() {
+        return numeroConto;
+    }
+
+    public String getTitolareConto() {
+        return titolareConto;
+    }
+
+    public void setTitolareConto(String titolareConto) {
+        this.titolareConto = titolareConto;
+    }
+
+    public double getSaldoConto() {
+        return saldoConto;
+    }
+
+    //metodo per versare denaro su conto
+    public void versamento(double aggiunta) {
+        saldoConto += aggiunta;
+        return;
+    }
+
+    //metodo per prelevare denaro
+    public boolean prelievo(double aggiunta) {
+        if (saldoConto >= aggiunta) {
+            saldoConto -= aggiunta;
+            return true;
+        } else {
+            System.out.println("Non hai abbastanza fondi.");
+            return false;
+        }
+    }
+
+    //metodo per acquisizione delle informazioni dsel conto
+    public String getInfoBankaccount() {
+        return "Numero Conto: " + numeroConto + ", Titolare Conto: " + titolareConto + "Il tuo Saldo: ";
+    }
+
+    public String getSaldoFormatted() {
+        return String.format("%.2f€", saldoConto);
+    }
 }
